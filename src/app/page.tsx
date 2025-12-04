cc93b0d7ca42b6fdefc085742112c3e679f19484
ashbi8137 <ashputhusseri@gmail.com>
2025-12-04 18:49:55 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen text-4xl font-bold">
-      Docker is Working !
+      Docker is Working !!
     </div>
   );
 }
